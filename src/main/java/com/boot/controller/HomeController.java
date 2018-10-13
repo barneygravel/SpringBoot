e25f45833d60e7b9d3ac8d8156f8fb060c72770d
Abhinav <abhinav000@gmail.com>
2018-10-14 04:24:44 +0530

    addeb web

---
+++ src/main/java/com/boot/controller/HomeController.java
@@ -0,0 +1,14 @@
+package com.boot.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+	
+	@RequestMapping("/")
+	public String hello() {
+		return "Hello World Spring Boot from Rest controller !!";
+	}
+
+}
